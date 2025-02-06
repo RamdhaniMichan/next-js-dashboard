a37583154610a2e973efc86b445d738c08ff0f4f
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2025-02-06 11:27:14 +0700

    complete course next-js template

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function Page() {
   return (
